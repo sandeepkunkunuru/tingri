14b6e843660bcc0b0b460db033582fc32b045585
sandeep kunkunuru <sandeep.kunkunuru@visualiq.com>
2014-06-06 16:45:19 -0400

    Adding gists 1) Tag without js 2) Invoke webservice from commandline 3) Ping website

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -5,6 +5,9 @@ class GistsController < ApplicationController
     :jvmmonitoring => "https://gist.github.com/sandeepkunkunuru/4725555.js",
     :thread => "https://gist.github.com/sandeepkunkunuru/6132857.js",
     :puppet => "https://gist.github.com/sandeepkunkunuru/7270672.js",
+    :pingwebsite => "https://gist.github.com/sandeepkunkunuru/040af3ad7788f154a176.js",
+    :tagwithoutjs => "https://gist.github.com/sandeepkunkunuru/c36267dc37e14b9e9479.js",
+    :webservice => "https://gist.github.com/sandeepkunkunuru/7031182.js",
   }
   
   def index
